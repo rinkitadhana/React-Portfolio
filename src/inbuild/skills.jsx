f3206441b53f42f298d2cd92e2d40111c2c7e683
Rinkit Adhana <therinkit@gmail.com>
2024-08-28 13:30:55 +0530

    skills-border

--- src/inbuild/skills.jsx
+++ src/inbuild/skills.jsx
@@ -38,89 +38,89 @@ const Skills = () => {
           <VscTools /> .
         </div>
         <div className=" mt-3 flex flex-wrap gap-1.5 dark:text-white text-gray-700">
-          <div className=" flex flex-row gap-1 rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20 cursor-pointer">
+          <div className=" flex flex-row gap-1 rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:border-white/20 dark:shadow-none  hover:bg-gray-200 dark:hover:bg-white/20 cursor-pointer">
             <RiHtml5Fill /> HTML
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FaCss3Alt /> CSS
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <DiJavascript /> JavaScript
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <TbBrandTypescript />
             TypeScript
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <RiTailwindCssFill /> Tailwind
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FaBootstrap /> Bootstrap
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <TbHexagonLetterM /> MaterialUI
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FaRegPlayCircle /> Flowbite
           </div>
 
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <RiReactjsLine /> ReactJs
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <RiNextjsLine />
             NextJs
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <IoLogoNodejs /> NodeJS
           </div>
 
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiExpress /> ExpressJS
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiPostman /> Postman
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiInsomnia /> Insomnia
           </div>
-          <div className=" flex flex-row gap-1 rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1 rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FiDatabase /> Database
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <BiLogoPostgresql /> PostgreSQL
           </div>
 
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiMongodb /> MongoDB
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FaGitAlt /> Git
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FaGithub /> GitHub
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <RiJavaLine /> Java
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <TbHexagonLetterC /> C
           </div>
 
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <TbBrandCpp /> C++
           </div>
 
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <VscTerminalLinux /> Linux
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <FiFigma /> Figma
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiNetlify /> Netlify
           </div>
-          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-none hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
+          <div className=" flex flex-row gap-1  rounded-md text-lg justify-center items-center font-medium w-fit px-2  dark:bg-white/30 shadow shadow-gray-200 border-2 border-gray-200 dark:shadow-none dark:border-white/20 hover:bg-gray-200 dark:hover:bg-white/20  cursor-pointer">
             <SiVercel /> Vercel
           </div>
         </div>
