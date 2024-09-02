76549859aad909c1a94f082492fa1c06e444ef85
Rinkit Adhana <therinkit@gmail.com>
2024-09-02 21:40:32 +0530

    navbar-routes

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -3,7 +3,18 @@ import { IoMenu } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const handleMultiClick = () => {
+    handleMenuOpen();
+    scrollToTop();
+  };
+
   return (
     <div className="sticky top-0 md:static">
       <nav className="flex justify-between items-center dark:text-white text-gray-700 py-4 px-8 md:px-40  md:mx-10  bg-white dark:bg-black z-50">
@@ -35,7 +46,7 @@ const Navbar = () => {
 
         <IoMenu
           className="xl:hidden block text-4xl cursor-pointer"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          onClick={handleMenuOpen}
         />
         <div
           className={` absolute xl:hidden top-16 left-0 w-full bg-white dark:bg-black dark:text-white flex flex-col items-center border-y dark:border-y-gray-500 drop-shadow-lg font-semibold text-base transform transition-transform ${
@@ -44,28 +55,28 @@ const Navbar = () => {
           style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
         >
           <Link
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onClick={handleMultiClick}
             to="/"
             className="list-none w-full text-center p-4 hover:bg-theme-blue hover:text-white transition-all cursor-pointer"
           >
             <li>Home</li>
           </Link>
           <Link
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onClick={handleMultiClick}
             to="/about"
             className="list-none w-full text-center p-4 hover:bg-theme-blue hover:text-white transition-all cursor-pointer"
           >
             <li>About</li>
           </Link>
           <Link
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onClick={handleMultiClick}
             to="/project"
             className="list-none w-full text-center p-4 hover:bg-theme-blue hover:text-white transition-all cursor-pointer"
           >
             <li>Projects</li>
           </Link>
           <Link
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            onClick={handleMultiClick}
             to="/blog"
             className="list-none w-full text-center p-4 hover:bg-theme-blue hover:text-white transition-all cursor-pointer"
           >
