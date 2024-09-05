bbc3b11e429efa4762d8180aa1b537a8d03d0241
Rinkit Adhana <therinkit@gmail.com>
2024-09-06 01:42:19 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,11 @@ function App() {
         <div className=" dark:bg-black min-h-screen">
           <BrowserRouter>
             <div className="flex gap-4 md:justify-between justify-end items-center pt-5 px-8 text-gray-700 dark:text-white">
-              <div className=" md:flex flex-row gap-2 text-base font-sans hidden font-medium  dark:text-white">
-                Local time <LiveClockUpdate />
+              <div className=" md:flex flex-row items-center gap-2 text-base font-sans hidden text-gray-500 font-medium  dark:text-gray-400">
+                <span>Local time</span>
+                <div className="text-sm ">
+                  <LiveClockUpdate />
+                </div>
               </div>
               <div className=" flex flex-row gap-3">
                 <span className="animate-pulse underline font-serif cursor-pointer text-pink-500 border-theme-blue hover:text-gray-700 dark:hover:text-white">
