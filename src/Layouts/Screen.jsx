b3c23f23bade41538f30e0a12f79b6a1b11f7917
Rinkit Adhana <therinkit@gmail.com>
2024-09-22 18:24:24 +0530

    screen-reload bug

--- src/Layouts/Screen.jsx
+++ src/Layouts/Screen.jsx
@@ -2,15 +2,15 @@ import { motion } from "framer-motion";
 
 const Screen = ({ children }) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
-      animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-      transition={{ duration: 0.8 }}
-    >
-      <div className="md:w-[865px] mx-auto text-gray-700 dark:text-white md:mt-10 mt-7 pb-20 font-popins px-2 md:px-0">
+    <div className="md:w-[865px] mx-auto text-gray-700 dark:text-white md:mt-10 mt-7 pb-20 font-popins px-2 md:px-0">
+      <motion.div
+        initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
+        animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+        transition={{ duration: 0.8 }}
+      >
         {children}
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
