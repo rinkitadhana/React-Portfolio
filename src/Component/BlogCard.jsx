b5a4cb03acac10dc8cbafb0e9828def0a676ea37
Rinkit Adhana <therinkit@gmail.com>
2024-10-20 12:36:09 +0530

    add

--- src/Component/BlogCard.jsx
+++ src/Component/BlogCard.jsx
@@ -1,7 +1,7 @@
 const BlogCard = ({ title, description, date }) => {
   return (
     <div className=" mt-4">
-      <div className="rounded-lg p-2 dark:hover:bg-white/20 hover:bg-black/10 transition-all delay-75 cursor-pointer">
+      <div className="rounded-lg p-2 dark:hover:bg-white/20 hover:bg-black/10 transition-all cursor-pointer">
         <h1 className="text-lg font-bold">{title}</h1>
         <p className=" text-sm opacity-80 ">{description}</p>
         <p className=" text-xs opacity-50">{date}</p>
