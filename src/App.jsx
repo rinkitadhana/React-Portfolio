1509e94d7a90d469100b24e46134521c608f51ed
Rinkit Adhana <therinkit@gmail.com>
2024-11-20 23:51:59 +0530

    OG update

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,22 @@ import ErrorMessage from "./pages/404"
 import Dark from "./tool/dark"
 import NavLayout from "./Layouts/NavLayout"
 import FirstBlog from "./Blog/TheDailyCrimes"
-import { HelmetProvider } from "react-helmet-async"
-import OGMeta from "./Component/OGMeta"
 
 function App() {
   return (
     <>
-      <HelmetProvider>
-        <OGMeta
-          title="Rinkit Adhana"
-          description="Check out my cool site!"
-          imageUrl="public\IMG\OG.png"
-        />
-        <Dark>
-          <Routes>
-            <Route path="/" element={<NavLayout />}>
-              <Route index element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/project" element={<Project />} />
-              <Route path="/blog" element={<Blog />} />
-            </Route>
-            <Route path="*" element={<ErrorMessage />} />
-            <Route path="blog/the-daily-crimes" element={<FirstBlog />} />
-          </Routes>
-        </Dark>
-      </HelmetProvider>
+      <Dark>
+        <Routes>
+          <Route path="/" element={<NavLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/blog" element={<Blog />} />
+          </Route>
+          <Route path="*" element={<ErrorMessage />} />
+          <Route path="blog/the-daily-crimes" element={<FirstBlog />} />
+        </Routes>
+      </Dark>
     </>
   )
 }
